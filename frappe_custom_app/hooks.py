12e1fda0b6b9376a6bb0727ec07bf80726cc436c
MUHAMMAD FAIZAN <muhammadfaizan.mf02@gmail.com>
2026-09-01 23:00:14 +0500

    Add pos_booking_stock_fix.js to app_include_js

--- frappe_custom_app/hooks.py
+++ frappe_custom_app/hooks.py
@@ -5,4 +5,4 @@ app_description = "Booking Order Automation for POS"
 app_email = "muhmmadfaizan.mf02@gmail.com"
 app_license = "MIT"
 
-app_include_js = ["/assets/frappe_custom_app/js/pos_booking.js"]
+app_include_js = ["/assets/frappe_custom_app/js/pos_booking.js", "/assets/frappe_custom_app/js/pos_booking_stock_fix.js"]
